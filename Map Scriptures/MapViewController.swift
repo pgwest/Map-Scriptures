f662a715a649c0c5bb6272260faa146601c50439
Peter West <apple@peters-macbook-pro-5.local>
2016-10-24 22:50:54 -0600

    Working pin (static pin) on map view, set up to receive geolocations

--- Map Scriptures/MapViewController.swift
+++ Map Scriptures/MapViewController.swift
@@ -11,14 +11,69 @@ import MapKit
 
 class MapViewController: UIViewController, MKMapViewDelegate {
 
+    
+    //Mark: - Outlets
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // Mark: - View Controller lifecycel
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.leftItemsSupplementBackButton = true
         navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
         
+
+        mapView.mapType = MKMapType.hybridFlyover
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.coordinate = CLLocationCoordinate2DMake(40.2506, -111.65247)
+        annotation.title = "Tanner Building"
+        annotation.subtitle = "BYU Campus"
+        
+        mapView.addAnnotation(annotation)
+        
+        
+        let camera = MKMapCamera(lookingAtCenter: CLLocationCoordinate2DMake(40.2506, -111.65247), fromEyeCoordinate: CLLocationCoordinate2DMake(40.2406, -111.65247), eyeAltitude: 300)
+        mapView.setCamera(camera, animated: true)
     }
 
+    @IBAction func setMapRegion(_ sender: AnyObject) {
+        let region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(40.2506, -111.65247), MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(region, animated: true)
+    }
+    
+    //Mark: - Map view delgate
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "Pin"
+        var view = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        
+        
+        if view == nil {
+            
+            let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            
+            pinView.canShowCallout = true
+            pinView.animatesDrop = true
+            pinView.pinTintColor = UIColor.red
+            
+            view = pinView
+            
+        } else {
+            view?.annotation = annotation
+        }
+        
+        return view
+    }
 
 }
 
--- Map Scriptures/ScriptureViewController.swift
+++ Map Scriptures/ScriptureViewController.swift
@@ -15,7 +15,11 @@ class ScriptureViewController : UIViewController, WKNavigationDelegate {
     var book: Book!
     var chapter = 0
     
+    private weak var mapViewController: MapViewController?
     private var webView: WKWebView!
+    
+    
+    // Mark: - View controller lifecycle
     
     override func loadView() {
         let webViewConfiguration = WKWebViewConfiguration()
@@ -27,6 +31,12 @@ class ScriptureViewController : UIViewController, WKNavigationDelegate {
         view = webView
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        configureDetailViewController()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,6 +44,68 @@ class ScriptureViewController : UIViewController, WKNavigationDelegate {
         
         webView.loadHTMLString(html, baseURL: nil)
     }
+    
+    
+    //Mark: - Segues
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Show Map" {
+            let navVC = segue.destination as? UINavigationController
+            if let mapVC = navVC?.topViewController as? MapViewController {
+                //NEEDSWORk: configure map view controller appropriately 
+            }
+            
+        }
+    }
+    
+    
+    
+    //Mark: - Web kit navicgation delegate
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        if let path = navigationAction.request.url?.absoluteString {
+            if path.hasPrefix(ScriptureRenderer.Constant.baseUrl) {
+                print("Request: \(path), mapViewController: \(mapViewController)")
+                
+                if let mapVC = mapViewController {
+                    //Needswork: zoom in on tapped geoplace
+                }
+                else{
+                    performSegue(withIdentifier: "Show Map", sender: self)
+                }
+                
+                decisionHandler(.cancel)
+            }
+        }
+        
+        decisionHandler(.allow)
+        
+    }
+    
+    
+    // Mark: - helpers
+    
+    func configureDetailViewController() {
+        
+        if let splitVC = splitViewController {
+            if let navVC = splitVC.viewControllers.last as? UINavigationController {
+                mapViewController = navVC.topViewController as? MapViewController
+            }
+            else {
+                mapViewController = splitVC.viewControllers.last as? MapViewController
+            }
+        }
+        else{
+            mapViewController = nil
+        }
+        
+        
+    }
+    
+    
+    
+    
     
     
 }
